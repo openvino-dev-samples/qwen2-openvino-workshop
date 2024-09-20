4ccb18041c3cb9b59e719494f4bfe3f5987998c0
ethan <ethan.yang@intel.com>
2024-09-20 02:23:55 -0700

    update requirements

--- run_lab.bat
+++ run_lab.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat" C:\Users\%USERNAME%\miniconda3\envs\openvino_devcon
+call "C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat" C:\Users\%USERNAME%\miniconda3\envs\openvino_qwen
 jupyter lab .
 pause
 call "C:\Users\%USERNAME%\miniconda3\Scripts\deactivate.bat"
--- setup_lab.bat
+++ setup_lab.bat
@@ -1,16 +1,14 @@
 @echo off
 
 echo Creating Python environment...
-call "C:\Users\%USERNAME%\miniconda3\Scripts\conda" create -n openvino_devcon python=3.10 -y
+call "C:\Users\%USERNAME%\miniconda3\Scripts\conda" create -n openvino_qwen python=3.10 -y
 
 echo Activating environment...
-call "C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat" openvino_devcon
+call "C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat" openvino_qwen
 
 echo Installing requirements...
 python -m pip install --upgrade pip
 pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-pip install --pre -U openvino --extra-index-url https://storage.openvinotoolkit.org/simple/wheels/nightly
-pip install --pre -U openvino-genai --extra-index-url https://storage.openvinotoolkit.org/simple/wheels/nightly
 
 echo Checking installed packages...
 conda list
