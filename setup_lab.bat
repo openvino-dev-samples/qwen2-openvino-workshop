d248d331977b10c3640601a3651968317cf911c2
Ethan Yang <ethan.yang@intel.com>
2024-10-17 17:30:16 +0800

    Update setup_lab.bat

--- setup_lab.bat
+++ setup_lab.bat
@@ -9,7 +9,6 @@ call "C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat" openvino_qwen
 echo Installing requirements...
 python -m pip install --upgrade pip
 pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-pip install --pre -U openvino-genai --extra-index-url https://storage.openvinotoolkit.org/simple/wheels/nightly
 
 echo Checking installed packages...
 conda list
